76459dc7db7dc0bc174da05e93c64707708b244a
Wiebke Schmitt <wiebke@notebook.com>
2020-08-06 11:04:10 +0200

    removed modifications for runtime measurement

--- primer.design.pipeline_jil_v10.3_standalone.R
+++ primer.design.pipeline_jil_v10.3_standalone.R
@@ -152,8 +152,11 @@ primer.design.pipeline<-function(table.in,
                                  add.ngs.adaptor.r="GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT", #adds this sequence 5' to primer2 to make it NGS compatible (optional) (character)
                                  #install.missing.packages=FALSE,
                                  create.graphics=TRUE, #will generate a graphical overview for each job with succesfull primerdesign(s) (TRUE/FALSE)
-                                 ...){          
- 
+                                 ...){      
+  
+  print("wbs start")
+  wbs_start <- Sys.time()
+  print(wbs_start)
   if(is.null(table.in) | is.null(path.out)){
   
     stop("input filename and export directory are missing.")  
@@ -3349,6 +3352,11 @@ log("done.")
 
 log("end.")
 
+print("wbs end")
+#print(Sys.time())
+wbs_end <- Sys.time()
+print(wbs_end)
+print(wbs_end - wbs_start)
 return(black)#output for primerwheel
 
 }#if(exists("results))
